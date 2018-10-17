60f361680539cbec622b8cc57284d9618a8d99a9
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2018-10-18 01:24:52 +0300

    modifications were made

--- src/main/java/com/github/unipi/trackandknow/Queries.java
+++ src/main/java/com/github/unipi/trackandknow/Queries.java
@@ -7,6 +7,11 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.geojson.Polygon;
+import com.mongodb.client.model.geojson.Position;
+import com.sun.tools.javac.util.List;
+
+import java.util.Arrays;
 
 import static com.mongodb.client.model.Filters.*;
 
@@ -18,15 +23,21 @@ public class Queries {
         MongoClientOptions options = MongoClientOptions.builder()/*.sslEnabled(true)*/.build();
         MongoClient mongoClient = new MongoClient(new ServerAddress("83.212.104.92", 27017), credential, options);
 
-        MongoCollection m = mongoClient.getDatabase("test").getCollection("points");
+        MongoCollection m = mongoClient.getDatabase("test").getCollection("geoPoints");
 
         long start = System.currentTimeMillis();
+
         //long d = m.count(and(and(and(gte("coordinates.0",21),gte("coordinates.1",35)),lte("coordinates.0",25)),lte("coordinates.1",38)));
-        long d = m.count(Filters.geoWithinBox("coordinates",0,0,1,1));
+        //long d = m.count(Filters.geoWithinBox("coordinates",0,0,1,1));
+
+        Polygon polygon = new Polygon(List.of(new Position(0, 0),new Position(1, 0),new Position(1, 1),new Position(0, 1),new Position(0, 0)));
+        long d = m.count(Filters.geoWithin("location.coordinates", polygon));
 
         System.out.println((System.currentTimeMillis()-start)/1000);
         System.out.println("d "+ d);
 
+
+        mongoClient.close();
 //        int count = 0;
 //
 //        while(d.iterator().hasNext()){
