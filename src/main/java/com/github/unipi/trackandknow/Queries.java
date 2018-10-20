817de0724a73460656668153ba44fd01a911bc25
nikolaos <nikos_koutroumanis@hotmail.com>
2018-10-20 22:46:11 +0300

    classes modified

--- src/main/java/com/github/unipi/trackandknow/Queries.java
+++ src/main/java/com/github/unipi/trackandknow/Queries.java
@@ -9,8 +9,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.geojson.Polygon;
 import com.mongodb.client.model.geojson.Position;
-import com.sun.tools.javac.util.List;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static com.mongodb.client.model.Filters.*;
@@ -21,20 +21,23 @@ public class Queries {
 
         MongoCredential credential = MongoCredential.createCredential("myUserAdmin", "test", "abc123".toCharArray());
         MongoClientOptions options = MongoClientOptions.builder()/*.sslEnabled(true)*/.build();
-        MongoClient mongoClient = new MongoClient(new ServerAddress("83.212.104.92", 27017), credential, options);
+        MongoClient mongoClient = new MongoClient(new ServerAddress("83.212.237.147", 27017), credential, options);
 
-        MongoCollection m = mongoClient.getDatabase("test").getCollection("geoPoints");
+        MongoCollection m = mongoClient.getDatabase("test").getCollection("points");
 
         long start = System.currentTimeMillis();
 
-        //long d = m.count(and(and(and(gte("coordinates.0",21),gte("coordinates.1",35)),lte("coordinates.0",25)),lte("coordinates.1",38)));
+        long d = m.count(and(and(and(gte("coordinates.0",21),gte("coordinates.1",35)),lte("coordinates.0",25)),lte("coordinates.1",38)));
         //long d = m.count(Filters.geoWithinBox("coordinates",0,0,1,1));
+        //long d = m.count(Filters.geoWithinCenterSphere("coordinates",0.5,0.5,0.3));
 
-        Polygon polygon = new Polygon(List.of(new Position(0, 0),new Position(1, 0),new Position(1, 1),new Position(0, 1),new Position(0, 0)));
-        long d = m.count(Filters.geoWithin("location.coordinates", polygon));
+        //Polygon polygon = new Polygon(Arrays.asList(new Position(21.5, 38),new Position(22, 38),new Position(22, 38.5),new Position(21.5, 38.5),new Position(21.5, 38)));
+        //long d = m.count(Filters.geoWithin("location.coordinates", polygon));
 
+        System.out.println("geopoints");
         System.out.println((System.currentTimeMillis()-start)/1000);
         System.out.println("d "+ d);
+
 
 
         mongoClient.close();
