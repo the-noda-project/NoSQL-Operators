271c37867dad7c0b23b9b890d60f510665c72fcf
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2018-10-29 00:19:25 +0200

    classes added, modifications were made, errors exist

--- src/main/java/com/github/unipi/trackandknow/nosqldbs/ConditionOperator.java
+++ src/main/java/com/github/unipi/trackandknow/nosqldbs/ConditionOperator.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class ConditionOperator<T> {
+public class ConditionOperator {
 
     private final ConditionOperatorType conditionOperatorType;
     private final List<ConditionOperator> conditionOperatorChildren;
