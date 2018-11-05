278db304f6ea758a9bb03c0036a3812ba393bd34
nikolaos <nikos_koutroumanis@hotmail.com>
2018-11-05 16:36:52 +0200

    Classes deleted

--- src/main/java/com/github/unipi/trackandknow/nosqldbs/NoSqlDbConnector.java
+++ src/main/java/com/github/unipi/trackandknow/nosqldbs/NoSqlDbConnector.java
@@ -2,7 +2,6 @@ package com.github.unipi.trackandknow.nosqldbs;
 
 public interface NoSqlDbConnector {
 
-    NoSqlDbManager getNoSqlDbManager();
     //NoSqlDbConnector connect();
 
 }
--- src/main/java/com/github/unipi/trackandknow/nosqldbs/mongodb/MongoDBOperators.java
+++ src/main/java/com/github/unipi/trackandknow/nosqldbs/mongodb/MongoDBOperators.java
@@ -1,15 +1,8 @@
 package com.github.unipi.trackandknow.nosqldbs.mongodb;
 
-import com.github.unipi.trackandknow.nosqldbs.ConditionOperator;
-import com.github.unipi.trackandknow.nosqldbs.ConditionOperatorType;
 import com.github.unipi.trackandknow.nosqldbs.NoSqlDbOperators;
 import com.github.unipi.trackandknow.nosqldbs.filteroperator.FilterOperator;
-import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import com.mongodb.client.model.Filters;
 
 import static com.mongodb.client.model.Filters.eq;
 
