a629c2d881c2c43e419bd926f54fe0404487b865
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-01-17 00:31:14 +0200

    Modifications were made

--- src/test/java/com/github/unipi/trackandknow/nosqldbs/nosqldbs/SpatioTemporalQueryTest.java
+++ src/test/java/com/github/unipi/trackandknow/nosqldbs/nosqldbs/SpatioTemporalQueryTest.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.Optional;
 
 import static com.github.unipi.trackandknow.nosqldbs.filterOperator.FilterOperators.gte;
+import static com.github.unipi.trackandknow.nosqldbs.filterOperator.FilterOperators.inGeoBox;
 import static com.github.unipi.trackandknow.nosqldbs.filterOperator.FilterOperators.inGeoCircleMeters;
 import static org.junit.Assert.*;
 
@@ -19,18 +20,17 @@ public class SpatioTemporalQueryTest {
     public void test() throws ParseException {
         NoSqlDbSystem.initialize();
 
-        Date d = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse("2018-09-01T00:00:00.000Z");
+        Date d = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse("2017-12-01T00:00:00.000Z");
 
         System.out.println(d);
         NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().host("83.212.102.163").database("test").username("myUserAdmin").password("abc123").port(28017).build();
 
-        System.out.println("count "+noSqlDbSystem.operateOn("geoPoints").filter(inGeoCircleMeters("location", Coordinates.newCoordinates(23.76,37.99),50)).count());
-        System.out.println("second count "+noSqlDbSystem.operateOn("geoPoints").filter(inGeoCircleMeters("location",Coordinates.newCoordinates(23.76,37.99),50), gte("date",d)).sum("date"));
+        System.out.println("count "+noSqlDbSystem.operateOn("geoPoints").filter(inGeoBox("location", Coordinates.newCoordinates(23.7576,37.9839),Coordinates.newCoordinates(23.7830,38.0050)),gte("date",d)).count());
 
 
-        Optional<Double> sum =noSqlDbSystem.operateOn("geoPoints").filter(inGeoCircleMeters("location",Coordinates.newCoordinates(23.76,37.99),50), gte("date",d)).sum("date");
+        System.out.println(noSqlDbSystem.operateOn("geoPoints").filter(inGeoCircleMeters("location",Coordinates.newCoordinates(23.76,37.99),200)/*, gte("date",d)*/).count());
 
-        System.out.println(sum.get());
 
+        noSqlDbSystem.closeConnection();
     }
 }
