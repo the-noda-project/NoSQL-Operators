407ea4a79b16bccbc81600b67ba6409c75ea21da
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-01-16 17:53:35 +0200

    Modifications were made

--- src/main/java/com/github/unipi/trackandknow/nosqldbs/filterOperator/geographicalOperator/GeographicalOperator.java
+++ src/main/java/com/github/unipi/trackandknow/nosqldbs/filterOperator/geographicalOperator/GeographicalOperator.java
@@ -25,10 +25,6 @@ public abstract class GeographicalOperator implements FilterOperator {
 
     protected void checkCoordinates() {
 
-        if (coordinatesArray == null) {
-            System.out.println("coordinatesArray is NULL");
-        }
-        System.out.println("coordinatesArray" + coordinatesArray.length);
         for (Coordinates c : coordinatesArray) {
             if (longitudeOutOfRange.test(c.getLongitude()) || latitudeOutOfRange.test(c.getLatitude())) {
                 try {
@@ -40,78 +36,6 @@ public abstract class GeographicalOperator implements FilterOperator {
         }
     }
 
-
-    //
-//
-//    Coordinates[] getCoordinatesArray();
-//    String getFieldName();
-//
-//    int getGeographicalOperatorId();
-//
-//    @Override
-//    default StringBuilder getJsonStringBuilder(){
-//        return formJsonStringOfGeographicalOperator();
-//    }
-//
-//    private StringBuilder formJsonStringOfGeographicalOperator(){
-//
-//        StringBuilder sb = new StringBuilder();
-//
-//        sb.append("{ ");
-//
-//        switch(getGeographicalOperatorId()){
-//            case 0 | 1:
-//
-//                if(!getFieldName().contains(".")){
-//                    sb.append(getFieldName());
-//                }
-//                else{
-//                    sb.append("\"" + getFieldName() + "\"");
-//                }
-//
-//                sb.append(": { $geoWithin: { $geometry: { type:\"Polygon\", coordinates:[ [");
-//
-//                for(Coordinates c : getCoordinatesArray()){
-//                    sb.append(" [");
-//                    sb.append(c.getLongitude());
-//                    sb.append(",");
-//                    sb.append(c.getLatitude());
-//
-//                    sb.append("]");
-//                    sb.append(",");
-//                }
-//
-//                sb.deleteCharAt(sb.lastIndexOf(", "));
-//
-//                sb.append("] ] } } } }");
-//                break;
-//
-//            case 2:
-//
-//                break;
-//            case 3:
-//
-//                break;
-//        }
-//                return sb;
-//    }
-//
-//    default void checkLonLatRange(T  longitude, T latitude)
-//    {
-//        if(longitudeOutOfRange.test(longitude) || latitudeOutOfRange.test(latitude)){
-//            try {
-//                throw new Exception("Longitude - Latitude values are incorrect");
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-////        if((Double.compare(longitude.doubleValue(),180)  == 1) || (Double.compare(longitude.doubleValue(),-180)  == -1)
-////        || (Double.compare(latitude.doubleValue(),90)  == 1) || (Double.compare(latitude.doubleValue(),-90)  == -1)){
-////
-////
-////        }
-//    }
-//
     Predicate<Double> longitudeOutOfRange = (longitude) -> ((Double.compare(longitude.doubleValue(), 180) == 1) || (Double.compare(longitude.doubleValue(), -180) == -1));
     Predicate<Double> latitudeOutOfRange = (latitude) -> ((Double.compare(latitude.doubleValue(), 90) == 1) || (Double.compare(latitude.doubleValue(), -90) == -1));
 }
