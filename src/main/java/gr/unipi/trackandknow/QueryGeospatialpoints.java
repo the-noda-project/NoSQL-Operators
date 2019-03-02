600abdebcc56260fa5cf8bab7b99f5fd06e08c43
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-03-02 20:53:48 +0200

    QueryGeospatialPoints modified

--- src/main/java/gr/unipi/trackandknow/QueryGeospatialpoints.java
+++ src/main/java/gr/unipi/trackandknow/QueryGeospatialpoints.java
@@ -7,6 +7,7 @@ import gr.unipi.trackandknow.api.nosqldb.NoSqlDbSystem;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 
 import static gr.unipi.trackandknow.api.filterOperator.FilterOperators.gte;
 import static gr.unipi.trackandknow.api.filterOperator.FilterOperators.lte;
@@ -21,14 +22,17 @@ public class QueryGeospatialpoints {
         final Date maxDate = sd.parse("2017-12-31 23:59:59.999");
         final Date minDate = sd.parse("2017-06-29 00:00:00.000");
 
+        int o = 0;
+
         if(Integer.valueOf(args[0])==0) {
             System.out.println("real");
             NoSqlDbSystem noSqlDbSystem3 = NoSqlDbSystem.MongoDB().host("localhost").database("real").username("real").password("real").port(27017).build();
             for (int i = 0; i < 3; i++) {
                 long start = System.currentTimeMillis();
-                noSqlDbSystem3.operateOn("geoPoints").filter(FilterOperators.inGeoBox("location", Coordinates.newCoordinates(23.6266, 37.9262), Coordinates.newCoordinates(23.6682, 37.9477)),gte("date",minDate),lte("date",maxDate)).count();
+                o = noSqlDbSystem3.operateOn("geoPoints").filter(FilterOperators.inGeoBox("location", Coordinates.newCoordinates(23.6266, 37.9262), Coordinates.newCoordinates(23.6682, 37.9477)),gte("date",minDate),lte("date",maxDate)).count();
                 System.out.println(((System.currentTimeMillis() - start) / 1000f) + " sec");
             }
+            System.out.println("real count: "+o);
             noSqlDbSystem3.closeConnection();
             System.out.println();
         }
@@ -39,9 +43,10 @@ public class QueryGeospatialpoints {
             NoSqlDbSystem noSqlDbSystem2 = NoSqlDbSystem.MongoDB().host("localhost").database("synthetic1").username("synthetic1").password("synthetic1").port(27017).build();
             for (int i = 0; i < 3; i++) {
                 long start = System.currentTimeMillis();
-                noSqlDbSystem2.operateOn("geoPoints").filter(FilterOperators.inGeoBox("location", Coordinates.newCoordinates(23.5500, 37.9262), Coordinates.newCoordinates(23.95, 38.30)),gte("date",minDate),lte("date",maxDate)).count();
+                o = noSqlDbSystem2.operateOn("geoPoints").filter(FilterOperators.inGeoBox("location", Coordinates.newCoordinates(23.5500, 37.9262), Coordinates.newCoordinates(23.95, 38.30)),gte("date",minDate),lte("date",maxDate)).count();
                 System.out.println(((System.currentTimeMillis() - start) / 1000f) + " sec");
             }
+            System.out.println("synthetic1 count: "+o);
             noSqlDbSystem2.closeConnection();
         }
 
@@ -50,9 +55,10 @@ public class QueryGeospatialpoints {
             NoSqlDbSystem noSqlDbSystem1 = NoSqlDbSystem.MongoDB().host("localhost").database("synthetic2").username("synthetic2").password("synthetic2").port(27017).build();
             for (int i = 0; i < 3; i++) {
                 long start = System.currentTimeMillis();
-                noSqlDbSystem1.operateOn("geoPoints").filter(FilterOperators.inGeoBox("location", Coordinates.newCoordinates(23.5500, 37.9262), Coordinates.newCoordinates(23.95, 38.30)),gte("date",minDate),lte("date",maxDate)).count();
+                o = noSqlDbSystem1.operateOn("geoPoints").filter(FilterOperators.inGeoBox("location", Coordinates.newCoordinates(23.5500, 37.9262), Coordinates.newCoordinates(23.95, 38.30)),gte("date",minDate),lte("date",maxDate)).count();
                 System.out.println(((System.currentTimeMillis() - start) / 1000f) + " sec");
             }
+            System.out.println("synthetic2 count: "+o);
             noSqlDbSystem1.closeConnection();
         }
 
