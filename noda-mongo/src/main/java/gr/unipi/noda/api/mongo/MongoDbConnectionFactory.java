c2c25c564d5dcdb82005346fedac69244cad8a5e
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-10-10 00:20:02 +0300

    NoSQL-Operators modifications Occurred

--- noda-mongo/src/main/java/gr/unipi/noda/api/mongo/MongoDbConnectionFactory.java
+++ noda-mongo/src/main/java/gr/unipi/noda/api/mongo/MongoDbConnectionFactory.java
@@ -4,10 +4,18 @@ import gr.unipi.noda.api.core.nosqldb.NoSqlConnectionFactory;
 import gr.unipi.noda.api.core.nosqldb.NoSqlDbConnector;
 import gr.unipi.noda.api.core.nosqldb.NoSqlDbOperators;
 import gr.unipi.noda.api.core.operators.aggregateOperators.BaseAggregateOperatorFactory;
-import gr.unipi.noda.api.mongo.aggregateOperator.MongoAggregateOperatorFactory;
+import gr.unipi.noda.api.core.operators.filterOperators.comparisonOperators.BaseComparisonOperatorFactory;
+import gr.unipi.noda.api.core.operators.filterOperators.geographicalOperators.BaseGeographicalOperatorFactory;
+import gr.unipi.noda.api.core.operators.filterOperators.logicalOperators.BaseLogicalOperatorFactory;
+import gr.unipi.noda.api.core.operators.sortOperators.BaseSortOperatorFactory;
+import gr.unipi.noda.api.mongo.aggregateOperator.MongoDBAggregateOperatorFactory;
+import gr.unipi.noda.api.mongo.filterOperator.comparisonOperator.MongoDBComparisonOperatorFactory;
+import gr.unipi.noda.api.mongo.filterOperator.geographicalOperator.MongoDBGeographicalOperatorFactory;
+import gr.unipi.noda.api.mongo.filterOperator.logicalOperator.MongoDBLogicalOperatorFactory;
+import gr.unipi.noda.api.mongo.sortOperator.MongoDBSortOperatorFactory;
 import org.apache.spark.sql.SparkSession;
 
-public final class MongoDbConnectionFactory extends NoSqlConnectionFactory {
+public final class MongoDBConnectionFactory extends NoSqlConnectionFactory {
     @Override
     public NoSqlDbConnector createNoSqlDbConnector(String host, int port, String username, String password, String database) {
         return MongoDBConnector.newMongoDBConnector(host, port, username, password, database);
@@ -49,7 +57,28 @@ public final class MongoDbConnectionFactory extends NoSqlConnectionFactory {
     }
 
     @Override
-    protected BaseAggregateOperatorFactory getAggregateOperatorFactory() {
-        return new MongoAggregateOperatorFactory<StringBuilder>();
+    protected BaseAggregateOperatorFactory getBaseAggregateOperatorFactory() {
+        return new MongoDBAggregateOperatorFactory();
     }
+
+    @Override
+    protected BaseComparisonOperatorFactory getBaseComparisonOperatorFactory() {
+        return new MongoDBComparisonOperatorFactory();
+    }
+
+    @Override
+    protected BaseGeographicalOperatorFactory getBaseGeographicalOperatorFactory() {
+        return new MongoDBGeographicalOperatorFactory();
+    }
+
+    @Override
+    protected BaseLogicalOperatorFactory getBaseLogicalOperatorFactory() {
+        return new MongoDBLogicalOperatorFactory();
+    }
+
+    @Override
+    protected BaseSortOperatorFactory getBaseSortOperatorFactory() {
+        return new MongoDBSortOperatorFactory();
+    }
+
 }
