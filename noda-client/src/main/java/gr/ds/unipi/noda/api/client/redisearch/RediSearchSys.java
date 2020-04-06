3267460cb4da97c9c3c78c756b6760c992c746a0
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-04-06 14:56:19 +0300

    RedisSearchSys sentines

--- noda-client/src/main/java/gr/ds/unipi/noda/api/client/redisearch/RediSearchSys.java
+++ noda-client/src/main/java/gr/ds/unipi/noda/api/client/redisearch/RediSearchSys.java
@@ -5,10 +5,15 @@ import gr.ds.unipi.noda.api.core.nosqldb.NoSqlDbConnector;
 import gr.ds.unipi.noda.api.redisearch.RediSearchConnectionFactory;
 import gr.ds.unipi.noda.api.redisearch.RediSearchConnector;
 import gr.ds.unipi.noda.api.redisearch.RediSearchOptions;
+import javafx.util.Pair;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.util.Pool;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RediSearchSys extends NoSqlDbSys {
 
@@ -22,6 +27,8 @@ public class RediSearchSys extends NoSqlDbSys {
     public static class Builder extends NoSqlDbSys.Builder<Builder>{
         private Pool<Jedis> jedisPool;
         private RediSearchOptions rediSearchOptions;
+        List<Pair<String,Integer>> sentinels = new ArrayList<>();
+
 
         public Builder(){
         }
@@ -37,6 +44,41 @@ public class RediSearchSys extends NoSqlDbSys {
             this.jedisPool = null;
             return this;
         }
+
+        public Builder sentinelHost(String host){
+
+            int i = 0;
+            while(i<sentinels.size()){
+                if(sentinels.get(i).getKey() == null){
+                    sentinels.set(i,new Pair<>(host, sentinels.get(i).getValue()));
+                    break;
+                }
+                i++;
+            }
+            if(i == sentinels.size()){
+                sentinels.add(new Pair<>(host,null));
+            }
+
+            return this;
+        }
+
+        public Builder sentinelPort(int port){
+
+            int i = 0;
+            while(i<sentinels.size()){
+                if(sentinels.get(i).getValue() == null){
+                    sentinels.set(i,new Pair<>(sentinels.get(i).getKey(), port));
+                    break;
+                }
+                i++;
+            }
+            if(i == sentinels.size()){
+                sentinels.add(new Pair<>(null,port));
+            }
+            return this;
+        }
+
+
 
         public Builder(Pool<Jedis> jedisPool) {
             this.rediSearchOptions = null;
@@ -56,6 +98,27 @@ public class RediSearchSys extends NoSqlDbSys {
 
     private RediSearchSys(Builder builder) {
         super(builder, new RediSearchConnectionFactory());
+
+        for(int i=0;i<builder.sentinels.size();i++){
+            if(builder.sentinels.get(i).getKey()==null){
+                builder.sentinels.set(i,new Pair<>("DEFAULT_HOST_OF_SENTINEL",builder.sentinels.get(i).getValue()));
+            }
+            if(builder.sentinels.get(i).getValue()==null){
+                builder.sentinels.set(i,new Pair<>(builder.sentinels.get(i).getKey(),"DEFAULT_PORT_OF_SENTINEL_INTEGER"));
+            }
+        }
+
+        if(getAddresses().size()==0){
+            builder.sentinels.add(new Pair<>("DEFAULT_HOST_OF_SENTINEL","DEFAULT_PORT_OF_SENTINEL_INTEGER"));
+        }
+
+        //auti i ekfrasi einai gia na tin peraseis ston connector kai na pareis ta sentinels
+        Collections.unmodifiableList(builder.sentinels.stream().distinct().collect(Collectors.toList()));
+        //ean yparxoyn sentinels tote to size tis listas tha einai 0 alliws ean yparxoyn tote >0
+
+
+
+
         connector = RediSearchConnector.newRediSearchConnector(getAddresses(), builder.rediSearchOptions, builder.jedisPool);
     }
 }
