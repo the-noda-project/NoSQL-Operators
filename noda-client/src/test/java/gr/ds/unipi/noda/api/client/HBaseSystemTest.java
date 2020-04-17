144834b6300233b2051566eb4662a2fcd769ffd2
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-04-18 00:33:29 +0300

    Hbase test polygon example added

--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/HBaseSystemTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/HBaseSystemTest.java
@@ -3,9 +3,7 @@ package gr.ds.unipi.noda.api.client;
 import gr.ds.unipi.noda.api.core.operators.filterOperators.geographicalOperators.Coordinates;
 import org.junit.Test;
 
-import static gr.ds.unipi.noda.api.core.operators.FilterOperators.eq;
-import static gr.ds.unipi.noda.api.core.operators.FilterOperators.gte;
-import static gr.ds.unipi.noda.api.core.operators.FilterOperators.inGeoRectangle;
+import static gr.ds.unipi.noda.api.core.operators.FilterOperators.*;
 
 public class HBaseSystemTest {
     @Test
@@ -13,7 +11,9 @@ public class HBaseSystemTest {
 
         NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.HBase().Builder().host("localhost").build();
         //noSqlDbSystem.operateOn("test").filter(eq("cf:name", "George")).project("cf:surname").printScreen();
-        noSqlDbSystem.operateOn("points").filter(inGeoRectangle("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(25,39))).printScreen();
+        //noSqlDbSystem.operateOn("points").filter(inGeoRectangle("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(25,39))).printScreen();
+        noSqlDbSystem.operateOn("points").filter(inGeoPolygon("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(24,39),Coordinates.newCoordinates(25,39),Coordinates.newCoordinates(25,38))).printScreen();
+
         //noSqlDbSystem.operateOn("points").filter(gte("location:lon",24)).printScreen();
         noSqlDbSystem.closeConnection();
     }
