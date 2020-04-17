3525c9d45644791f43d6c1ef498987e24c7971c7
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-04-17 12:01:41 +0300

    rectangle example on hbase

--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/HBaseSystemTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/HBaseSystemTest.java
@@ -1,15 +1,18 @@
 package gr.ds.unipi.noda.api.client;
 
+import gr.ds.unipi.noda.api.core.operators.filterOperators.geographicalOperators.Coordinates;
 import org.junit.Test;
 
 import static gr.ds.unipi.noda.api.core.operators.FilterOperators.eq;
+import static gr.ds.unipi.noda.api.core.operators.FilterOperators.inGeoRectangle;
 
 public class HBaseSystemTest {
     @Test
     public void hBaseTest() {
 
         NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.HBase().Builder().host("localhost").build();
-        noSqlDbSystem.operateOn("test").filter(eq("cf:name", "George")).project("cf:surname").printScreen();
+        //noSqlDbSystem.operateOn("test").filter(eq("cf:name", "George")).project("cf:surname").printScreen();
+        noSqlDbSystem.operateOn("test").filter(inGeoRectangle("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(25,39))).printScreen();
         noSqlDbSystem.closeConnection();
     }
 
