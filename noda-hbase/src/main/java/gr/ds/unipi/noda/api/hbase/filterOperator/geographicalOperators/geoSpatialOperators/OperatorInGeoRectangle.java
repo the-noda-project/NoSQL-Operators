30066f51969b6d62fe6dcbb46304cb62a294bee1
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-04-17 12:42:24 +0300

    polygon modified

--- noda-hbase/src/main/java/gr/ds/unipi/noda/api/hbase/filterOperator/geographicalOperators/geoSpatialOperators/OperatorInGeoRectangle.java
+++ noda-hbase/src/main/java/gr/ds/unipi/noda/api/hbase/filterOperator/geographicalOperators/geoSpatialOperators/OperatorInGeoRectangle.java
@@ -17,6 +17,11 @@ final class OperatorInGeoRectangle extends GeoSpatialOperator<Rectangle> {
 
     @Override
     protected Filter geometryRefactor() {
+        System.out.println((Filter) and(gte(getFieldName() + ":lon", getGeometry().getLowerBound().getLongitude()),
+                gte(getFieldName() + ":lat", getGeometry().getLowerBound().getLatitude()),
+                lte(getFieldName() + ":lon", getGeometry().getUpperBound().getLongitude()),
+                lte(getFieldName() + ":lat", getGeometry().getUpperBound().getLatitude())).getOperatorExpression());
+
         return (Filter) and(gte(getFieldName() + ":lon", getGeometry().getLowerBound().getLongitude()),
                 gte(getFieldName() + ":lat", getGeometry().getLowerBound().getLatitude()),
                 lte(getFieldName() + ":lon", getGeometry().getUpperBound().getLongitude()),
