fb3e1ade9f84d7c8b123cd9929ef168b56f7d99a
Nikos Kousathanas <nikolaos.kousathanas@gmail.com>
2020-05-10 14:32:19 +0300

    s

--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/NoSqlDbSystemTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/NoSqlDbSystemTest.java
@@ -20,23 +20,29 @@ public class NoSqlDbSystemTest {
 //        noSqlDbSys.closeConnection();
 //    }
 
-//    @Test
-//    public void neo4jTest() {
-//        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().Builder("nikos", "b.xT9aop6yHVKR.VT55Sd4YB7ICbuR6").host("hobby-mhkjmndbhbhagbkeckbgfoel.dbs.graphenedb.com").port(24787).build();
-//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT", -38.31416)).printScreen();
-//        noSqlDbSystem.closeConnection();
-//    }
-
-
     @Test
-    public void groupingAndsortingExample() {
-        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().Builder("neo4j", "nikos").host("localhost").port(7687).build();
-//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT", -38.31416)).printScreen();
-        noSqlDbSystem.operateOn("Ship").filter(inGeoCircleKm("LOCATION", Coordinates.newCoordinates(145.00441, -38.31416), 0.04)).printScreen();
-//        noSqlDbSystem.operateOn("Ship").filter(or(eq("LAT",-38.31416), eq("LON",145.004403333), gt("SPEED", 20))).groupBy("LAT", max("COURSE")).printScreen();
-
+    public void neo4jTest() {
+        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().Builder("neo4j", "nikos", "graph").host("127.0.0.1").port(7687).build();
+        noSqlDbSystem.operateOn("Ship").filter(eq("LAT", -38.31416)).printScreen();
         noSqlDbSystem.closeConnection();
     }
+
+
+//    @Test
+//    public void groupingAndsortingExample() {
+//        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().username("neo4j").password("nikos").host("localhost").port(7687).database("graph").build();
+//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT","'-38.31416'")).groupBy("fieldA", max("fieldB")).sort(asc("fieldC")).printScreen();
+//        noSqlDbSystem.closeConnection();
+//    }
+//
+//    @Test
+//    public void countExample() {
+//        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().username("neo4j").password("nikos").host("localhost").port(7687).database("graph").build();
+//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT","'-38.31416'")).groupBy("fieldA", count()).printScreen();
+//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT","'-38.31416'")).count();
+//
+//        noSqlDbSystem.closeConnection();
+//    }
 
 
 
