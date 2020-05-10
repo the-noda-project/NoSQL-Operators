c9c75df1fd32b1fca480b32e36b4964159c3f484
Nikos Kousathanas <nikolaos.kousathanas@gmail.com>
2020-05-10 14:47:30 +0300

    s

--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/NoSqlDbSystemTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/NoSqlDbSystemTest.java
@@ -1,18 +1,18 @@
 package gr.ds.unipi.noda.api.client;
 
-import gr.ds.unipi.noda.api.core.operators.filterOperators.geographicalOperators.Coordinates;
-import gr.ds.unipi.noda.api.core.operators.filterOperators.geographicalOperators.geometries.Circle;
-import javafx.geometry.Point2D;
+import gr.ds.unipi.noda.api.core.operators.aggregateOperators.AggregateOperator;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriterSettings;
 import org.junit.Test;
 
-import java.awt.*;
 import java.util.Arrays;
 
-import static gr.ds.unipi.noda.api.core.operators.FilterOperators.*;
-import static gr.ds.unipi.noda.api.core.operators.AggregateOperators.*;
-import static gr.ds.unipi.noda.api.core.operators.SortOperators.*;
+import static gr.ds.unipi.noda.api.core.operators.FilterOperators.eq;
 
 public class NoSqlDbSystemTest {
+
 //    public void neo4j(){
 //        NoSqlDbSystem noSqlDbSys =  NoSqlDbSystem.Neo4j().Builder("neo4j","nikos","graph").host("127.0.0.1").port(7687).build();
 //        //noSqlDbSys.operateOn("Ship").filter(or(eq("LAT",-38.31416), eq("LON",145.004403333), gt("SPEED", 20))).filter(eq("LAT",-38.31416)).sort(desc("COURSE")).sort(asc("LON")).printScreen();
@@ -20,25 +20,29 @@ public class NoSqlDbSystemTest {
 //        noSqlDbSys.closeConnection();
 //    }
 
-//    @Test
-//    public void neo4jTest() {
-//        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().Builder("nikos", "b.xT9aop6yHVKR.VT55Sd4YB7ICbuR6").host("hobby-mhkjmndbhbhagbkeckbgfoel.dbs.graphenedb.com").port(24787).build();
-//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT", -38.31416)).printScreen();
-//        noSqlDbSystem.closeConnection();
-//    }
-
-
     @Test
-    public void groupingAndsortingExample() {
-        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().Builder("neo4j", "nikos", "jbjb").host("localhost").port(7687).build();
-//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT", -38.31416)).printScreen();
-        noSqlDbSystem.operateOn("Ship").filter(inGeoCircleKm("LOCATION", Coordinates.newCoordinates(145.00441, -38.31416), 0.04)).printScreen();
-//        noSqlDbSystem.operateOn("Ship").filter(or(eq("LAT",-38.31416), eq("LON",145.004403333), gt("SPEED", 20))).groupBy("LAT", max("COURSE")).printScreen();
-
+    public void neo4jTest() {
+        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().Builder("neo4j", "nikos", "graph").host("127.0.0.1").port(7687).build();
+        noSqlDbSystem.operateOn("Ship").filter(eq("LAT", -38.31416)).printScreen();
         noSqlDbSystem.closeConnection();
     }
 
 
+//    @Test
+//    public void groupingAndsortingExample() {
+//        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().username("neo4j").password("nikos").host("localhost").port(7687).database("graph").build();
+//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT","'-38.31416'")).groupBy("fieldA", max("fieldB")).sort(asc("fieldC")).printScreen();
+//        noSqlDbSystem.closeConnection();
+//    }
+//
+//    @Test
+//    public void countExample() {
+//        NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Neo4j().username("neo4j").password("nikos").host("localhost").port(7687).database("graph").build();
+//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT","'-38.31416'")).groupBy("fieldA", count()).printScreen();
+//        noSqlDbSystem.operateOn("Ship").filter(eq("LAT","'-38.31416'")).count();
+//
+//        noSqlDbSystem.closeConnection();
+//    }
 
     @Test
     public void check() {
@@ -69,5 +73,41 @@ public class NoSqlDbSystemTest {
 
     public static void something2(String a, String b, String[] c) {
         Arrays.stream(new String[]{a, b}).toArray(String[]::new);
+    }
+
+    @Test
+    public void check2(){
+
+        String s = "{ $group: " + "{ _id: {"+ "myF: "+ "\"" + "$" + "myF" + "\"" +
+                ",fd: "+ "\"" + "$" + "fd" + "\"" +
+                "}, er:5,  " +"} }";
+
+        Bson bson = (Bson) Document.parse(s);
+
+//        System.out.println(((Document) bson).containsKey("$group"));
+//        System.out.println(((Document) bson).get("$group"));
+//
+//        Document id = (Document) ((Document) ((Document) bson).get("$group")).get("_id");
+//
+//        System.out.println("_id: "+ id);
+//
+//
+//        ((Document) ((Document) bson).get("$group")).remove("_id");
+//        Document perisevoumena = (Document) ((Document) ((Document) bson).get("$group"));
+//        System.out.println("perisevoumena "+ perisevoumena.isEmpty());
+//        perisevoumena.forEach((o,v)-> System.out.println(o+" "+v));
+//
+//        ((Document) ((Document) bson).get("$group")).append("_id", id).append("s","d").append("r","d");
+
+
+        String json = ((Document) bson).toJson();
+        System.out.println(json);
+
+        System.out.println(Document.parse(json.substring(0, json.length()-3) +", count:{ $sum:1 } }}").toJson());
+
+
+//        String json = ((Document)bson).toJson(JsonWriterSettings.builder().outputMode(JsonMode.RELAXED).build());
+//
+//        System.out.println(json.substring(0, json.length()-3));
     }
 }
