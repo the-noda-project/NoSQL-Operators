9e982a443822685b6433d21edd80b26792d5834d
Nikos Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-09-09 18:21:48 +0300

    Update NoSqlDbConnectionManager.java

--- noda-core/src/main/java/gr/ds/unipi/noda/api/core/nosqldb/NoSqlDbConnectionManager.java
+++ noda-core/src/main/java/gr/ds/unipi/noda/api/core/nosqldb/NoSqlDbConnectionManager.java
@@ -5,21 +5,29 @@ import java.util.Map;
 
 public abstract class NoSqlDbConnectionManager<T> {
 
-    private final Map<NoSqlDbConnector<T>, T> connections;
+    private final Map<NoSqlDbConnector, T> connections;
 
     protected NoSqlDbConnectionManager() {
         connections = new HashMap<>();
     }
 
-    protected Map<NoSqlDbConnector<T>, T> getConnections() {
+    protected Map<NoSqlDbConnector, T> getConnections() {
         return connections;
     }
 
     public T getConnection(NoSqlDbConnector<T> noSqlDbConnector) {
-        return connections.computeIfAbsent(noSqlDbConnector, NoSqlDbConnector::createConnection);
+
+        if (connections.containsKey(noSqlDbConnector)) {
+            return connections.get(noSqlDbConnector);
+        }
+
+        connections.put(noSqlDbConnector, noSqlDbConnector.createConnection());
+
+        return connections.get(noSqlDbConnector);
+
     }
 
-    public abstract boolean closeConnection(NoSqlDbConnector<T> noSqlDbConnector);
+    public abstract boolean closeConnection(NoSqlDbConnector noSqlDbConnector);
 
     public abstract boolean closeConnections();
 
