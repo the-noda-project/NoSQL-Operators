9f9be6a9a75a108736d1de4604c34e2183621d8b
Nikos Kousathanas <nikolaos.kousathanas@gmail.com>
2020-10-14 22:31:11 +0300

    rectangle and circle changes

--- noda-neo4j/src/main/java/gr.ds.unipi.noda.api.neo4j/filterOperators/geoperators/geoTemporalOperators/OperatorInGeoTemporalRectangle.java
+++ noda-neo4j/src/main/java/gr.ds.unipi.noda.api.neo4j/filterOperators/geoperators/geoTemporalOperators/OperatorInGeoTemporalRectangle.java
@@ -29,8 +29,8 @@ final class OperatorInGeoTemporalRectangle extends GeoTemporalOperator<Rectangle
         SmallHilbertCurve f = HilbertCurve.small().bits(bits).dimensions(3);
 
 
-        long[] point1 = scalePoint(getGeographicalOperator().getGeometry().getMbr().getLowerBound().getLatitude(), getGeographicalOperator().getGeometry().getMbr().getLowerBound().getLongitude(), getTemporalType().getLowerBound().getTime(), 1567296299, 1569887618, maxOrdinates);
-        long[] point2 = scalePoint(getGeographicalOperator().getGeometry().getMbr().getUpperBound().getLatitude(), getGeographicalOperator().getGeometry().getMbr().getUpperBound().getLongitude(), getTemporalType().getUpperBound().getTime(), 1567296299, 1569887618, maxOrdinates);
+        long[] point1 = scalePoint(getGeographicalOperator().getGeometry().getMbr().getLowerBound().getLatitude(), getGeographicalOperator().getGeometry().getMbr().getLowerBound().getLongitude(), getTemporalType().getLowerBound().getTime(), 1546992000, 1554854399, maxOrdinates);
+        long[] point2 = scalePoint(getGeographicalOperator().getGeometry().getMbr().getUpperBound().getLatitude(), getGeographicalOperator().getGeometry().getMbr().getUpperBound().getLongitude(), getTemporalType().getUpperBound().getTime(), 1546992000, 1554854399, maxOrdinates);
 //// return just one range
         System.out.println("POINT 1 = " + point1);
         System.out.println("POINT 2 = " + point2);
@@ -43,7 +43,13 @@ final class OperatorInGeoTemporalRectangle extends GeoTemporalOperator<Rectangle
             System.out.println(range.low());
             System.out.println(range.high());
 
-            sb.append("s.STHilbertIndex > " + low + " AND s.STHilbertIndex < " + high + " WITH s WHERE point({ srid:4326 , x: " + getGeographicalOperator().getGeometry().getLowerBound().getLongitude() + ", y: "+ getGeographicalOperator().getGeometry().getLowerBound().getLatitude() +" }) < s." + getGeographicalOperator().getFieldName() + " < point({ srid: 4326 , x: " + getGeographicalOperator().getGeometry().getUpperBound().getLongitude() + ", y: "+ getGeographicalOperator().getGeometry().getUpperBound().getLatitude() + " }) AND " + getTemporalType().getLowerBound().getTime() + " < s." + getTemporalFieldName()  + " < " + getTemporalType().getUpperBound().getTime() );
+            if(low != high) {
+                sb.append("s.STHilbertIndex > " + low + " AND s.STHilbertIndex < " + high + " WITH s WHERE point({ srid:7203 , x: " + getGeographicalOperator().getGeometry().getLowerBound().getLatitude() + ", y: "+ getGeographicalOperator().getGeometry().getLowerBound().getLongitude() +" }) < s." + getGeographicalOperator().getFieldName() + " < point({ srid: 7203 , x: " + getGeographicalOperator().getGeometry().getUpperBound().getLatitude() + ", y: "+ getGeographicalOperator().getGeometry().getUpperBound().getLongitude() + " }) AND " + getTemporalType().getLowerBound().getTime() + " < s." + getTemporalFieldName()  + " < " + getTemporalType().getUpperBound().getTime() );
+
+            } else {
+                sb.append("s.STHilbertIndex = " + low + " WITH s WHERE point({ srid:7203 , x: " + getGeographicalOperator().getGeometry().getLowerBound().getLatitude() + ", y: "+ getGeographicalOperator().getGeometry().getLowerBound().getLongitude() +" }) < s." + getGeographicalOperator().getFieldName() + " < point({ srid: 7203 , x: " + getGeographicalOperator().getGeometry().getUpperBound().getLatitude() + ", y: "+ getGeographicalOperator().getGeometry().getUpperBound().getLongitude() + " }) AND " + getTemporalType().getLowerBound().getTime() + " < s." + getTemporalFieldName()  + " < " + getTemporalType().getUpperBound().getTime() );
+
+            }
 
         });
 
