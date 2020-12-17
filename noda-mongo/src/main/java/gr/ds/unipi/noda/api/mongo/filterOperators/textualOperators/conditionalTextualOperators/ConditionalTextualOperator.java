e798613e61bab5ba41a56bd3a40f5536387751d9
knesto <kw.stas91@hotmail.com>
2020-12-17 09:37:01 +0200

    New classes on MongoDB and Hbase for Spatio-Textual process

---
+++ noda-mongo/src/main/java/gr/ds/unipi/noda/api/mongo/filterOperators/textualOperators/conditionalTextualOperators/ConditionalTextualOperator.java
@@ -0,0 +1,11 @@
+package gr.ds.unipi.noda.api.mongo.filterOperators.textualOperators.conditionalTextualOperators;
+
+public abstract class ConditionalTextualOperator extends gr.ds.unipi.noda.api.core.operators.filterOperators.textualOperators.conditionalTextualOperators.ConditionalTextualOperator<Object> {
+    public ConditionalTextualOperator(String fieldName, String[] elements) {
+        super(fieldName, elements);
+    }
+
+     public Object getOperatorExpression(){
+        return null;
+     }
+}
---
+++ noda-mongo/src/main/java/gr/ds/unipi/noda/api/mongo/filterOperators/textualOperators/conditionalTextualOperators/OperatorAnyKeywords.java
@@ -0,0 +1,15 @@
+package gr.ds.unipi.noda.api.mongo.filterOperators.textualOperators.conditionalTextualOperators;
+
+import gr.ds.unipi.noda.api.core.operators.filterOperators.textualOperators.TextualOperator;
+
+public final class OperatorAnyKeywords extends ConditionalTextualOperator {
+
+	protected  OperatorAnyKeywords(String fieldName, String[] elements){
+        super(fieldName, elements);
+    }
+
+    public static OperatorAnyKeywords newOperatorAnyKeywords(String fieldName, String keyword, String... keywords){
+        return new OperatorAnyKeywords(fieldName, TextualOperator.oneKeywordsAtLeastToArray(keyword, keywords));
+    }
+
+}
