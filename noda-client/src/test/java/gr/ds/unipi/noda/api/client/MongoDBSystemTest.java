614e004795949f4e448947057d6800d5b3df32e1
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2021-03-07 23:14:08 +0200

    Compatibility with mongodb and spark fixed

--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/MongoDBSystemTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/MongoDBSystemTest.java
@@ -15,19 +15,9 @@ public class MongoDBSystemTest {
                 .getOrCreate();
 
         NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().Builder("myUserAdmin","myPass","admin").host("localhost").sparkSession(spark).build();
-        //noSqlDbSystem.operateOn("points").filter(gte("cf:name", 43.7)).project("cf:name").printScreen();
-        //noSqlDbSystem.operateOn("points").filter(inGeoRectangle("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(25,39))).printScreen();
-        //noSqlDbSystem.operateOn("points").filter(inGeoPolygon("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(24,39),Coordinates.newCoordinates(25,39),Coordinates.newCoordinates(25,38))).printScreen();
-
         NoSqlDbOperators noSqlDbOperators = noSqlDbSystem.operateOn("inventory");
 
         noSqlDbOperators.toDataframe().show();
-
-//        Dataset<Row> d1 = a.toDataframe();
-//
-//        a = noSqlDbOperators.filter(inGeoRectangle("location", Coordinates.newCoordinates(24,38),Coordinates.newCoordinates(25,39)));
-//        Dataset<Row> d2  = a.toDataframe();
-
         noSqlDbSystem.closeConnection();
 
     }
