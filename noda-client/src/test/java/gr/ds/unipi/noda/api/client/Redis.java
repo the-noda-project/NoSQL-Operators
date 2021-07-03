ed64260e42c51f0ad3ea240fceab1d96092005e8
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2021-07-03 16:13:44 +0300

    new class added

---
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/Redis.java
@@ -0,0 +1,37 @@
+package gr.ds.unipi.noda.api.client;
+
+import gr.ds.unipi.noda.api.core.nosqldb.NoSQLExpression;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Redis {
+    @Ignore
+@Test
+public void redisTest() throws ParseException {
+
+    SparkSession spark = SparkSession
+            .builder()
+            .appName("redis-df")
+            .master("local[*]")
+            .config("spark.redis.host", "localhost")
+            .config("spark.redis.port", "6379")
+            .getOrCreate();
+
+    NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.Redis().Builder().port(6379).sparkSession(spark).soTimeout(0).connectionTimeout(0).build();
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+
+    Date d1 = sdf.parse("2019-11-11T11:00:00.000");
+    Date d2 = sdf.parse("2019-11-11T11:30:00.000");
+
+    noSqlDbSystem.operateOn("passengerCars").toDataframe().show();
+
+    System.out.println(NoSQLExpression.INSTANCE.getExpression());
+    noSqlDbSystem.closeConnection();
+}
+}
